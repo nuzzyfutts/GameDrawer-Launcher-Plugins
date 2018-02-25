49b6f33acd8577afadfac35a83ebc1b0c78ced15
Aimal Wajihuddin <aimal@wajihudd.in>
2018-02-25 14:46:47 -0500

    Fixed IO related bugs in both files

--- Battlenet.lua
+++ Battlenet.lua
@@ -6,121 +6,92 @@
 ]]
 
 function main(BNET_CONFIG_FILE_PATH)
-	
-		-- Hard coded because this is standard throughout all systems (with C being the primary drive)
-		local BNET_CONFIG_FILE_PATH = "C:\\Users\\Aimal\\AppData\\Roaming\\Battle.net\\battle.net.config"
 
-		--Dictionary to convert names of games installed found in Battle.net.config to appID names to launch through the Blizzard App Desktop API
-		local LIST_OF_GAMES = {prometheus="Pro",destiny2="DST2",hero="Hero",hearthstone="WTCG",diablo3="D3",warcraft="WoW",starcraft2="S2",starcraft2="S1"}
-		
-		--Dictionary to convert names found to names to be displayed in GameDrawer
-		local GAME_NAMES = {prometheus="Overwatch",destiny2="Destiny 2",hero="Heroes of the Storm",hearthstone="Hearthstone",diablo3="Diablo 3",warcraft="World of Warcraft",starcraft2="Starcraft 2",starcraft2="Starcraft"}
-		
-		--Hard coded URLs to download banners from Blizzard's website since they don't have a system like Steam
-		local BANNER_URLS = {prometheus="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-overwatch.jpg",destiny2="https://bnetproduct-a.akamaihd.net//ff5/c9fb7c865fa0eb80b1cacef42dd3cb1e-feature-07.png",herp="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-heroes.jpg",hearthstone="http://us.blizzard.com/static/_images/games/hearthstone/gamecard-games-hearthstone-en.jpg",diablo3="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-d3.jpg",warcraft="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-wow.jpg",starcraft2="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-sc2.jpg",starcraft2="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-sc1.jpg"}
-	
+	local BNET_CONFIG_FILE_PATH = "C:\\Users\\Aimal\\AppData\\Roaming\\Battle.net\\battle.net.config"
 
-		--[[
-			Function to get the list of installed games from Battle.net.config
-		]]
-		local function getInstalledGames()
-			local familyFile = io.open(BNET_CONFIG_FILE_PATH,"r")	--Open the file so it can be read
-			local count = 0											--Counter to keep track of which line we are on (so we can exit)
-			local found = false										--Boolean to not string.match every time on line 42
-			local level = 0											--How many levels ({) deep arae we in the file
-			local start = nil										--What the line of starting of the 'Games' entry is (so we can exit)
-			local entry = nil										--What the current entry is (used for keeping track of actual games)
-			local games = {}										--The list of all games to be returned later
-	
-			if familyFile then
-				for line in io.lines(BNET_CONFIG_FILE_PATH) do
-					
-					count = count + 1
-	
-					--If we are encountering the Games entry for the first time or if we have found it already
-					if found or string.match(line,'Games') then
-	
-						--Look for entries in the games section that match the syntax for a game entry
-						entry = string.match(line,'^%s*"([%w%p]+)": {')
-	
-						--ignore battle_net and prometheus because they can't be launched through Blizzard Desktop API
-						if entry ~= nil and level == 1 and entry ~= "battle_net" and entry ~= "prometheus_test" then
-							table.insert(games, entry)
-						end
-	
-						--increment level if we have a { on this line}
-						if string.match(line, "{") then
-							level = level + 1
-						end
-	
-						--decrement level if we have a } on this line
-						if string.match(line, "}") then
-							level = level - 1
-						end
-	
-						--if first run through, make found = true and set start line
-						if not found then
-							found = true
-							start = count
-						end
-						
-						--if we have exited the games entry in the json file, exit the loop
-						if level == 0 and start ~= count then
-							break
-						end
+	local LIST_OF_GAMES = {prometheus="Pro",destiny2="DST2",hero="Hero",hearthstone="WTCG",diablo3="D3",warcraft="WoW",starcraft2="S2",starcraft2="S1"}
+	local GAME_NAMES = {prometheus="Overwatch",destiny2="Destiny 2",hero="Heroes of the Storm",hearthstone="Hearthstone",diablo3="Diablo 3",warcraft="World of Warcraft",starcraft2="Starcraft 2",starcraft2="Starcraft"}
+	local BANNER_URLS = {prometheus="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-overwatch.jpg",destiny2="https://bnetproduct-a.akamaihd.net//ff5/c9fb7c865fa0eb80b1cacef42dd3cb1e-feature-07.png",herp="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-heroes.jpg",hearthstone="http://us.blizzard.com/static/_images/games/hearthstone/gamecard-games-hearthstone-en.jpg",diablo3="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-d3.jpg",warcraft="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-wow.jpg",starcraft2="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-sc2.jpg",starcraft2="http://us.blizzard.com/static/_images/lang/en-us/gamecard-games-sc1.jpg"}
+
+	local function getInstalledGames()
+		local familyFile = io.open(BNET_CONFIG_FILE_PATH,"r")
+		local count = 0
+		local found = false
+		local level = 0
+		local start = nil
+		local count = 0
+		local entry = nil
+		local uid = nil
+		local games = {}
+
+		if familyFile then
+			for line in familyFile:lines() do
+				
+				count = count + 1
+
+				if found or string.match(line,'Games') then
+
+					entry = string.match(line,'^%s*"([%w%p]+)": {')
+
+					if entry ~= nil and level == 1 and entry ~= "battle_net" and entry ~= "prometheus_test" then
+						table.insert(games, entry)
+					end
+
+					if string.match(line, "{") then
+						level = level + 1
+					end
+
+					if string.match(line, "}") then
+						level = level - 1
+					end
+
+					if not found then
+						found = true
+						start = count
+					end
+
+					if level == 0 and start ~= count then
+						break
 					end
 				end
-			else
-				--Battle.net.config not found file not found
-				debug("Error: Could not find/open the file 'battle.net.config'")
-				return nil
+				familyFile:close()
 			end
-			return games
-		end
-		
-		local function finalizeGames(games)
-	
-			local resultTable = {}		--final table of all finalized games
-			local currTable = {}		--Current table to keep track of individual games one at a time
-			local currGame = nil
-	
-			--Assign all required values for ecah game
-			for i=1, table.getn(games) do
-				currGame = games[i]
-				currTable["appID"]= LIST_OF_GAMES[currGame]							--The unique appID for each game
-				currTable["appName"] = GAME_NAMES[currGame]							--The name of each game that will be displayed/user facing
-				currTable["installed"] = true										--Initialized value for consistency with launchers
-				currTable["hidden"] = false											--Initialized default value for use with GameDrawer
-				currTable["lastPlayed"] =  nil										--Initialized for use with GameDrawer
-				currTable["appPath"] = "battlenet://"..LIST_OF_GAMES[games[i]]		--Use of Blizzard desktop API
-				currTable["bannerURL"] = BANNER_URLS[currGame]						--URLs of banners from Blizzard's website
-				currTable["bannerName"] = LIST_OF_GAMES[currGame]..".jpg"			--The name that each banner will have
-				currTable["launcher"] = "battlenet"									--The launcher of current game. For use with GameDrawer
-	
-				--Insert current game table into final table
-				table.insert(resultTable, currTable)
-
-				--Reset table for potential debugging
-				currTable = {}
-	
-			end
-
-			if resultTable == {} then
-				debug("No installed games found for Blizzard App")
-				return nil
-			else
-				return resultTable
-			end
-		end
-	
-		local installedGames = getInstalledGames()
-
-		if installedGames == nil then
-			local final = nil
 		else
-			local final = finalizeGames(installedGames)
+			--game family file not found file not found
+			debug("Error: Could not find/open the file 'battle.net.config'")
+			return nil
 		end
-		
-		return final
-	
+		return games
 	end
+	
+	local function finalizeGames(games)
+
+		local resultTable = {}
+		local currTable = {}
+
+		for i=1, table.getn(games) do
+			currTable["appID"]= LIST_OF_GAMES[games[i]]
+			currTable["appName"] = GAME_NAMES[games[i]]
+			currTable["installed"] = true
+			currTable["hidden"] = false
+			currTable["lastPlayed"] =  nil
+			currTable["appPath"] = "battlenet://"..LIST_OF_GAMES[games[i]]
+			currTable["bannerURL"] = BANNER_URLS[games[i]]
+			currTable["bannerName"] = LIST_OF_GAMES[games[i]]..".jpg"		--TODO
+			currTable["launcher"] = "battlenet"
+
+			table.insert(resultTable, currTable)
+			currTable = {}
+
+		end
+
+		return resultTable
+
+	end
+
+	local installedGames = getInstalledGames()
+	local final = finalizeGames(installedGames)
+
+	return final
+
+end
--- Steam.lua
+++ Steam.lua
@@ -6,192 +6,195 @@
 			{string} USER_ID - The user's steam ID
 ]]
 
-function main(DEFAULT_STEAM_PATH, USER_ID)		-- For use in game drawer
-	--function main(args)								-- For internal testing
-	
-		local DEFAULT_STEAM_PATH = args[1]
-		local USER_ID = args[2]
-	
-		local function getIDsAndLastPlayed()
-			local LOCAL_CONFIG_VDF_PATH = DEFAULT_STEAM_PATH.."userdata\\"..USER_ID.."\\config\\localconfig.vdf"
-			local vdfFile = io.open(LOCAL_CONFIG_VDF_PATH,"r")
-			local count = 0								--used to keep track of lines
-			local found = false							--used for easier check of if we are in the apps section
-			local level = 0								--used to keep track how deep in entries we currently are
-			local currApp = {}							--used to keep track of data for current app
-			local apps = {}								--used for storing data of all apps
-			local previousLine = ""
-	
-			if vdfFile then
-				for line in io.lines(LOCAL_CONFIG_VDF_PATH) do
-	
-					count = count + 1
-					line = string.lower(line)
-					if found or string.match(line,'%s*apps') then
-	
-						--=====================================================================================
-						--                                        DEBUG
-						--=====================================================================================
-						--This line is used for debugging
-						--It will print every line in terminal, as well as log it in
-						--rainmeter log to assist debugging
-						--debug(line)
-	
-						--increment/decrement level to check for closing of individual apps
-						--entries and for checking when apps section has ended
-						if string.match(line,"^%s*{") then				--if on a line where an app entry is beginning
-							level = level + 1
-							previousLine = "open"
+--function main(DEFAULT_STEAM_PATH, USER_ID)		-- For use in game drawer
+function main(args)								-- For internal testing
+
+	local DEFAULT_STEAM_PATH = args[1]
+	local USER_ID = args[2]
+
+	local function getIDsAndLastPlayed()
+		local LOCAL_CONFIG_VDF_PATH = DEFAULT_STEAM_PATH.."userdata\\"..USER_ID.."\\config\\localconfig.vdf"
+		local vdfFile = io.open(LOCAL_CONFIG_VDF_PATH,"r")
+		local count = 0								--used to keep track of lines
+		local found = false							--used for easier check of if we are in the apps section
+		local level = 0								--used to keep track how deep in entries we currently are
+		local currApp = {}							--used to keep track of data for current app
+		local apps = {}								--used for storing data of all apps
+		local previousLine = ""
+
+		if vdfFile then
+			for line in vdfFile:lines() do
+
+				count = count + 1
+				line = string.lower(line)
+				if found or string.match(line,'%s*apps') then
+
+					--=====================================================================================
+					--                                        DEBUG
+					--=====================================================================================
+					--This line is used for debugging
+					--It will print every line in terminal, as well as log it in
+					--rainmeter log to assist debugging
+					--debug(line)
+
+					--increment/decrement level to check for closing of individual apps
+					--entries and for checking when apps section has ended
+					if string.match(line,"^%s*{") then				--if on a line where an app entry is beginning
+						level = level + 1
+						previousLine = "open"
+					end
+
+					--if on a line where an app entry is ending or line before is entry and this line is an entry
+					if string.match(line,"^%s*}") or (previousLine == "id" and string.match(line,'^%s*"(%d+)"') ~= nil) then
+
+						--only decrement level if only first case is satisfied and second case is false
+						if string.match(line,"^%s*}") then
+							level = level - 1
 						end
-	
-						--if on a line where an app entry is ending or line before is entry and this line is an entry
-						if string.match(line,"^%s*}") or (previousLine == "id" and string.match(line,'^%s*"(%d+)"') ~= nil) then
-	
-							--only decrement level if only first case is satisfied and second case is false
-							if string.match(line,"^%s*}") then
-								level = level - 1
+						if level == 1 then
+							currApp["appID"] = appID
+							table.insert(apps,currApp)
+
+							--if the game has never been played
+							if currApp.lastPlayed == nil then
+								currApp.lastPlayed = 0				--set lastplayed timestamp to 0
 							end
-							if level == 1 then
-								currApp["appID"] = appID
-								table.insert(apps,currApp)
-	
-								--if the game has never been played
-								if currApp.lastPlayed == nil then
-									currApp.lastPlayed = 0				--set lastplayed timestamp to 0
-								end
-								
-								--=====================================================================================
-								--                                        DEBUG
-								--=====================================================================================
-								--commented debug code to print all appIDs and lastPlayed timestamps
-								--debug("appID: "..currApp.appID,"Last Played: "..currApp.lastPlayed)
-	
-								currApp = {}
-								previousLine = "close"
-							end
+							
+							--=====================================================================================
+							--                                        DEBUG
+							--=====================================================================================
+							--commented debug code to print all appIDs and lastPlayed timestamps
+							--debug("appID: "..currApp.appID,"Last Played: "..currApp.lastPlayed)
+
+							currApp = {}
+							previousLine = "close"
 						end
+					end
+					
+					--individual game parameters capture
+					local temp = string.match(line,'^%s*"(%d+)"')
+					if temp ~= nil then
+						appID = temp
+						previousLine = "id"
+					end
+					
+					--if currently in an app entry
+					if level == 2 then
+						local title = string.match(line,'^%s*"(%w*)"')
+
+						--if title of current line is lastplayed, get timestamp data
+						if title == "lastplayed" then
+							currApp["lastPlayed"] = string.match(line,'.*"(%d*)"')
+						end
+						previousLine = "dataEntry"
+					end
+
+					--sets start line for check of closing of apps section
+					if not found then
+						previousLine = "begin"
+						found = true
+						start = count
+					end
+
+					--checks to stop reading file if apps section has ended
+					if level == 0  and start ~= count then
+						vdfFile:close()
+						break
+					end
+				end
+			end
+	else
+		--VDF file not found... Return nil for check further on in the script
+		return nil
+	end
+		return apps
+	end
+
+	local function getAllDirectories()
+		local LIBRARY_FOLDERS_VDF_PATH = DEFAULT_STEAM_PATH.."steamapps\\libraryfolders.vdf"
+		local libVDFFile = io.open(LIBRARY_FOLDERS_VDF_PATH,"r")				--open libraryfolders.vdf
+		local dirs = {DEFAULT_STEAM_PATH}										--initialize dirs table with default steam directory preinserted
+
+		--if vdf file exists
+		if libVDFFile then
+
+			--iterate through all the lines
+			for line in libVDFFile:lines() do
+				local dirline = string.match(line,'^%s*"%d+"%s*"(.*)"')			--match format for only lines with directories
+				
+				--if that format is on this line
+				if dirline then
+					newdirline = string.gsub(dirline,'\\\\','\\').."\\"
+					table.insert(dirs,newdirline)								--insert that path into the table
+				end
+				libVDFFile:close()
+			end
+		end
+		return dirs																--return table of directories
+	end
+
+	local function checkAppManifests(directories,appTable)
+		local resultTable = {}
+		local currTable = {}
+
+		--iterate through all steam directories
+		for i=1,table.getn(directories) do
+
+			--iterate through all app possibilities in each directory
+			for curr = 1, table.getn(appTable) do
+				local currManifest = directories[i].."steamapps/appmanifest_"..appTable[curr].appID..".acf"		--generate filepath for app
+				manifestFile = io.open(currManifest,"r")														--open file
+				
+				--check if file exists
+				if manifestFile then
+
+					--iterate through all lines
+					for line in manifestFile:lines() do
+						line = string.gsub(line,'[^%w%s%p]+',"")
+						appName = string.match(line,'.*"name"%s*"(.*)"')										--obtain app name from file
 						
-						--individual game parameters capture
-						local temp = string.match(line,'^%s*"(%d+)"')
-						if temp ~= nil then
-							appID = temp
-							previousLine = "id"
-						end
-						
-						--if currently in an app entry
-						if level == 2 then
-							local title = string.match(line,'^%s*"(%w*)"')
-	
-							--if title of current line is lastplayed, get timestamp data
-							if title == "lastplayed" then
-								currApp["lastPlayed"] = string.match(line,'.*"(%d*)"')
-							end
-							previousLine = "dataEntry"
-						end
-	
-						--sets start line for check of closing of apps section
-						if not found then
-							previousLine = "begin"
-							found = true
-							start = count
-						end
-	
-						--checks to stop reading file if apps section has ended
-						if level == 0  and start ~= count then
+						--if this line contains the name field
+						if appName then
+							currTable["appID"] = appTable[curr].appID
+							currTable["lastPlayed"] = appTable[curr].lastPlayed
+							currTable["appName"] = appName														--set appName in appTable
+							currTable["installed"] = true														--set installed var in appTable (only for consistency across launchers)
+							currTable["hidden"] = false															--PLACEHOLDER/INITIAL ASSIGNMENT parameter for if game should be hidden
+							currTable["appPath"] = "steam://rungameid/"..appTable[curr].appID
+                            currTable["bannerURL"] = "http://cdn.akamai.steamstatic.com/steam/apps/"..appTable[curr].appID.."/header.jpg"
+                            currTable["bannerName"] = appTable[curr].appID..".jpg"
+							currTable["launcher"] = "steam"														--defines which launcher this game is from
+							table.insert(resultTable,currTable)
+							currTable = {}
 							break
 						end
-					end
-				end
-		else
-			--VDF file not found... Return nil for check further on in the script
-			return nil
-		end
-			return apps
-		end
-	
-		local function getAllDirectories()
-			local LIBRARY_FOLDERS_VDF_PATH = DEFAULT_STEAM_PATH.."steamapps\\libraryfolders.vdf"
-			local libVDFFile = io.open(LIBRARY_FOLDERS_VDF_PATH,"r")				--open libraryfolders.vdf
-			local dirs = {DEFAULT_STEAM_PATH}										--initialize dirs table with default steam directory preinserted
-	
-			--if vdf file exists
-			if libVDFFile then
-	
-				--iterate through all the lines
-				for line in io.lines(LIBRARY_FOLDERS_VDF_PATH) do
-					local dirline = string.match(line,'^%s*"%d+"%s*"(.*)"')			--match format for only lines with directories
-					
-					--if that format is on this line
-					if dirline then
-						newdirline = string.gsub(dirline,'\\\\','\\').."\\"
-						table.insert(dirs,newdirline)								--insert that path into the table
+						manifestFile:close()
 					end
 				end
 			end
-			return dirs																--return table of directories
 		end
-	
-		local function checkAppManifests(directories,appTable)
-			local resultTable = {}
-			local currTable = {}
-	
-			--iterate through all steam directories
-			for i=1,table.getn(directories) do
-	
-				--iterate through all app possibilities in each directory
-				for curr = 1, table.getn(appTable) do
-					local currManifest = directories[i].."steamapps/appmanifest_"..appTable[curr].appID..".acf"		--generate filepath for app
-					manifestFile = io.open(currManifest,"r")														--open file
-					
-					--check if file exists
-					if manifestFile then
-	
-						--iterate through all lines
-						for line in io.lines(currManifest) do
-							line = string.gsub(line,'[^%w%s%p]+',"")
-							appName = string.match(line,'.*"name"%s*"(.*)"')										--obtain app name from file
-							
-							--if this line contains the name field
-							if appName then
-								currTable["appID"] = appTable[curr].appID
-								currTable["lastPlayed"] = appTable[curr].lastPlayed
-								currTable["appName"] = appName														--set appName in appTable
-								currTable["installed"] = true														--set installed var in appTable (only for consistency across launchers)
-								currTable["hidden"] = false															--PLACEHOLDER/INITIAL ASSIGNMENT parameter for if game should be hidden
-								currTable["appPath"] = "steam://rungameid/"..appTable[curr].appID
-								currTable["bannerURL"] = "http://cdn.akamai.steamstatic.com/steam/apps/"..appTable[curr].appID.."/header.jpg"
-								currTable["bannerName"] = appTable[curr].appID..".jpg"
-								currTable["launcher"] = "steam"														--defines which launcher this game is from
-								table.insert(resultTable,currTable)
-								currTable = {}
-								break
-							end
-						end
-					end
-				end
-			end
-			return resultTable																						--return fully populated appTable
-		end
-	
-		local dirs = getAllDirectories()
-		local ids = getIDsAndLastPlayed()
-		if ids == nil then
-			local final = nil
-			debug("VDF file not found... Stopping search")
-		else
-			local final = checkAppManifests(dirs,ids)
-		end
-		--=====================================================================================
-		--                                        DEBUG
-		--=====================================================================================
-		--Debug code to log all found appNames, lastPlayed timestamps, and appIDs
-		--[[for a = 1, table.getn(final) do
-			debug(final[a].appName.."",final[a].lastPlayed.."",final[a].appID.."","")
-		end
-		debug("Total number of games found: ",table.getn(final))]]
-	
-		return final
+		return resultTable																						--return fully populated appTable
 	end
-	
-	--test command for debugging on my machine with my tools
-	--steam "C:\Program Files (x86)\Steam\" 75154095
+
+	local dirs = getAllDirectories()
+	local ids = getIDsAndLastPlayed()
+	if ids == nil then
+		local final = nil
+		debug("VDF file not found... Stopping search")
+	else
+		local final = checkAppManifests(dirs,ids)
+	end
+	--=====================================================================================
+	--                                        DEBUG
+	--=====================================================================================
+	--Debug code to log all found appNames, lastPlayed timestamps, and appIDs
+	for a = 1, table.getn(final) do
+		debug(final[a].appName.."",final[a].lastPlayed.."",final[a].appID.."","")
+	end
+	debug("Total number of games found: ",table.getn(final))
+
+	return final
+end
+
+--test command for debugging on my machine with my tools
+--Steam "C:\Program Files (x86)\Steam\" 75154095
